bf756ab0b4dcd7db79597767b80ba6ee90a89e39
vaibhav <prince123Tiwari@outlook.com>
2022-07-10 20:25:22 -0700

    first commit

--- login.php
+++ login.php
@@ -41,7 +41,7 @@ session_start();
 <?php
 
 if(isset($_POST["submit"])){
-$username=$_POST["username"];
+$username=$_POST["username"]; 
 $email=$_POST["email"];
 $password=$_POST["password"];
 $sql="SELECT * FROM `loginuser` WHERE `USERNAME`= '$username' && `EMAIL`= '$email'";
